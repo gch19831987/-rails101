cf122455944f86ee762177627ebe35c677a988ca
zqz <71264737@qq.com>
2016-12-30 13:48:23 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,8 +9,8 @@ class GroupsController < ApplicationController
 
  def show
    @group = Group.find(params[:id])
+   @posts = @group.posts
  end
-
  def edit
    find_group_and_check_permission
  end
