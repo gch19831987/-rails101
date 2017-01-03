291cbfd763601a570823e549a76168d3da6e35f3
zqz <71264737@qq.com>
2017-01-03 17:08:12 +0800

    add error

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -1,7 +1,9 @@
 class Account::GroupsController < ApplicationController
-  before_action :authenticate_user!
-
   def index
-    @groups = current_user.participated_groups
-  end
+  @groups = Group.all
+end
+
+ def new
+  @group = Group.new
+ end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -67,19 +67,21 @@ end
     private
 
 
+     def create
+       @group = Group.new(group_params)
+     if  @group.save
+         redirect_to groups_path
+	else 
 
-def create
-  @group = Group.new(group_params)
-   @group.user = current_user
+	render :new
+end
+     end
 
-  if @group.save
-    current_user.join!(@group)
-    redirect_to groups_path
-  else
-    render :new
-  end
- end
+     private
 
+     def group_params
+       params.require(:group).permit(:title, :description)
+     end
  private
 
  def find_group_and_check_permission
